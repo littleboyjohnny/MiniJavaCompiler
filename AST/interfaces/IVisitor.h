db047cbda82ad58f658e100880ab155fc391ce31
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2018-10-28 17:18:13 +0300

    Solve cross-dependence problem

--- AST/interfaces/IVisitor.h
+++ AST/interfaces/IVisitor.h
@@ -1,9 +1,11 @@
 #ifndef COMPILERAST_IVISITOR_H
 #define COMPILERAST_IVISITOR_H
 
+class IAcceptable;
+
 class IVisitor {
 public:
-    virtual void Visit() = 0; //// TODO: добавить перегрузку для каждого нетерминала
+    virtual void Visit( IAcceptable* acceptable ) = 0; //// TODO: добавить перегрузку для каждого нетерминала
 };
 
 #endif //COMPILERAST_IVISITOR_H
