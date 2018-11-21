3f9e26c467f59de2655c9fdd3dfbc9edbd564539
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2018-11-21 22:52:59 +0300

    Add parameters support

--- SymbolTable/include/MethodInfo.h
+++ SymbolTable/include/MethodInfo.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <unordered_map>
 #include <memory>
+#include <vector>
 
 class CMethodInfo : public IInfo {
 public:
@@ -15,9 +16,16 @@ public:
 
     CBlockScope* GetScope();
 
+    void RegisterAsParameter( const CSymbol* symbol );
+    bool TryRegisterAsParameter( const CSymbol* symbol );
+
+    const CVariableInfo* TryResolveParameter( int paramIndex );
+    const CVariableInfo* TryResolveParameter( const CSymbol* paramSymbol );
+
 private:
     const CSymbol* name;
     const CSymbol* retTypeName;
+    std::vector<const CSymbol*> parameters;
 
     std::unique_ptr<CBlockScope> scope;
 };
--- SymbolTable/src/MethodInfo.cpp
+++ SymbolTable/src/MethodInfo.cpp
@@ -1,6 +1,7 @@
 #include <include/MethodInfo.h>
 
 #include <cassert>
+#include <algorithm>
 
 
 CMethodInfo::CMethodInfo( const CSymbol* _name, const CSymbol* _retTypeName ) :
@@ -22,3 +23,46 @@ CBlockScope* CMethodInfo::GetScope()
     return scope.get();
 }
 
+const CVariableInfo* CMethodInfo::TryResolveParameter( int paramIndex )
+{
+    assert( paramIndex >= 0 );
+
+    if( paramIndex < parameters.size() ) {
+        const CSymbol* paramSymbol = parameters[paramIndex];
+        return scope->TryResolveVariable( paramSymbol );
+    } else {
+        return nullptr;
+    }
+}
+
+const CVariableInfo* CMethodInfo::TryResolveParameter( const CSymbol* paramSymbol )
+{
+    assert( paramSymbol != nullptr );
+
+    auto it = std::find( std::begin( parameters ), std::end( parameters ), paramSymbol );
+    if( it != std::end( parameters ) ) {
+        return scope->TryResolveVariable( paramSymbol );
+    } else {
+        return nullptr;
+    }
+}
+
+void CMethodInfo::RegisterAsParameter( const CSymbol* symbol )
+{
+    assert( symbol != nullptr );
+
+    parameters.push_back( symbol );
+}
+
+bool CMethodInfo::TryRegisterAsParameter( const CSymbol* symbol )
+{
+    assert( symbol != nullptr );
+
+    auto it = std::find( std::begin( parameters ), std::end( parameters ), symbol );
+    if( it != std::end( parameters ) ) {
+        RegisterAsParameter( symbol );
+        return true;
+    } else {
+        return false;
+    }
+}
