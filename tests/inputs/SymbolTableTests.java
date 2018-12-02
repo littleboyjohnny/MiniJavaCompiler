757f80fca03994b4862706717e808e06b1f791b5
Ilya Belkin <ilya.belkin-trade@yandex.ru>
2018-12-02 18:01:31 +0300

    Add test file

---
+++ tests/inputs/SymbolTableTests.java
@@ -0,0 +1,28 @@
+class Factorial{
+    public static void main(String[] a){
+    }
+}
+
+class Fac {
+
+    public int ComputeFac(int num){
+	    int num_aux ;
+	    int Fac;
+	    if (num < 1)
+	        num_aux = 1 ;
+	    else
+	        num_aux = num * (this.ComputeFac(1)) ;
+	    if (Fac) {
+
+        } else {
+
+        }
+	    return num_aux ;
+
+    }
+
+    public int Fac() {
+        return 0;
+    }
+
+}
